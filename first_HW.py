69711175bb54ba272e6220d09f8ce77c2e9a6d88
Maxim Mikhailovich <suhochev_m@mail.ru>
2023-05-30 06:01:01 +0300

    first commit to home work 2

--- first_HW.py
+++ first_HW.py
@@ -1,2 +1,3 @@
 print("Hi, it`s my first home work")
 print("use git branch --set-upstream-to=origin/<branch> master")
+print("Этот файл будет являться основой для работы с GitHub и во втором домашнем задании")
